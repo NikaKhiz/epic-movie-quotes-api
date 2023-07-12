b353bd23200af75d05a931a0876c98382d217e31
NikaKhiz <nika.khizambareli@redberry.ge>
2023-07-12 13:52:12 +0400

    feat: quote model add quote migration file create factory and seeder for quote add relations between quote and movies and modifie database seeders accordingly

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Spatie\Translatable\HasTranslations;
 
 class Movie extends Model
@@ -19,6 +20,11 @@ class Movie extends Model
 	public function user(): BelongsTo
 	{
 		return $this->belongsTo(User::class);
+	}
+
+	public function quotes(): HasMany
+	{
+		return $this->hasMany(Quote::class);
 	}
 
 	public function genres(): BelongsToMany
---
+++ app/Models/Quote.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Spatie\Translatable\HasTranslations;
+
+class Quote extends Model
+{
+	use HasFactory,HasTranslations;
+
+	protected $guarded = ['id'];
+
+	public $translatable = ['title'];
+
+	public function user(): BelongsTo
+	{
+		return $this->belongsTo(User::class);
+	}
+
+	public function movie(): BelongsTo
+	{
+		return $this->belongsTo(Movie::class);
+	}
+}
