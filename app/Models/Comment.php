a5e2eab2fd95b2a19013eb70536dc6f3a01f6bf2
NikaKhiz <nika.khizambareli@redberry.ge>
2023-07-13 13:26:23 +0400

    feat: comment model and seeders

---
+++ app/Models/Comment.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Comment extends Model
+{
+	use HasFactory;
+
+	protected $with = ['user'];
+
+	protected $guarded = ['id'];
+
+	public function user(): BelongsTo
+	{
+		return $this->belongsTo(User::class);
+	}
+
+	public function quote(): BelongsTo
+	{
+		return $this->belongsTo(Quote::class);
+	}
+}
---
+++ database/factories/CommentFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Comment>
+ */
+class CommentFactory extends Factory
+{
+	/**
+	 * Define the model's default state.
+	 *
+	 * @return array<string, mixed>
+	 */
+	public function definition(): array
+	{
+		return [
+			'user_id' => 1,
+			'quote_id'=> fake()->randomDigit() + 1,
+			'comment' => fake()->sentence(),
+		];
+	}
+}
